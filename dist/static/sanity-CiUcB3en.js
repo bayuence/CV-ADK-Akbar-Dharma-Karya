c314aea1d4ce5e3fc92a950c9c41902f1c871d8d
bayuence <bayuence@gmail.com>
2026-02-15 15:11:49 +0700

    chore: add sanity.cli.ts for studio deployment

---
+++ dist/static/sanity-CiUcB3en.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-zebQ7tDq.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
---
+++ sanity.cli.ts
@@ -0,0 +1,11 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "9l4mv13h",
+    dataset: "production",
+  },
+  deployment: {
+    appId: "bufgihae66s3c38xn415oqz3",
+  },
+});
